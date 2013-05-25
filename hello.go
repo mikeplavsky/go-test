e183271c01b701977fb8a23db741f0d1021fb38b
Mike Plavsky <Mike.Plavsky@quest.com>
2013-05-25 11:08:17 +0000

    enum

--- hello.go
+++ hello.go
@@ -1,6 +1,12 @@
 package main
 import "fmt"
 
+const (
+  Red             = (1<<iota)
+  Green           = (1<<iota)
+  Blue, ColorMask = (1<<iota), (1<<(iota+1))-1
+)
+
 func printf(str string, args ...interface{})(int, error) {
   _,err := fmt.Printf(str, args...)
   return len(args), err
