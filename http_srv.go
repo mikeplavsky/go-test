e8601b200323b1872d23ee02ba71b065cdb2a926
Mike Plavsky <Mike.Plavsky@quest.com>
2013-05-27 22:25:18 +0000

    http

---
+++ http_srv.go
@@ -0,0 +1,19 @@
+package main
+import (
+  "fmt"
+  "net/http"
+  "log"
+)
+
+type Hello struct {}
+
+func (Hello) ServeHTTP(
+  w http.ResponseWriter,
+  r *http.Request){
+  fmt.Fprint(w, "Hello, World!\n")
+}
+
+func main() {
+  var h Hello
+  log.Fatal(http.ListenAndServe(":80", h))
+}
